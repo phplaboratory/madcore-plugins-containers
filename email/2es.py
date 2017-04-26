b08605a2b4ccfcca55faadc2e0a3593b629fd978
Peter Styk <peter@styk.tv>
2017-04-26 11:15:05 +0100

    playing with email headers

--- email/2es.py
+++ email/2es.py
@@ -2,12 +2,18 @@
 from flanker.addresslib import address
 from flanker import mime
 import os
+import json
 
 for root, dirs, files in os.walk("/Users/polfilm/.mail/account/allmail/cur"):
     for file in files:
         filefullname = os.path.join(root, file)
         message_string = open(filefullname,"rb").read()
         msg = mime.from_string(message_string)
-        print len(msg.headers)
+        print
+        print "SUBJECT: {0}".format(msg.clean_subject)
+        for header, value in msg.headers.iteritems():
+            print "    HEADER {0}".format(header)
+            print "    VALUE  {0}".format(value)
+            print
 
 
